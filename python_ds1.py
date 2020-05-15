5ac7212de96539fca67c9252ac23921a04ec62c1
Salman Ibne Eunus <salman.ibne.eunus@g.bracu.ac.bd>
2020-05-15 09:57:05 +0600

    DONE

---
+++ python_ds1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 07:28:51 2020
+
+@author: salman
+"""
+word = 'banana'
+
+count = 0
+
+for lame in word:
+    if lame=='a':
+        count = count + 1
+        
+print(count)        
