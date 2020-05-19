939e98e49a7ca8b9a54a35c255076f658dfd625c
Salman Ibne Eunus <salman.ibne.eunus@g.bracu.ac.bd>
2020-05-19 22:28:22 +0600

    DONE

--- python_ds1.py
+++ python_ds1.py
@@ -4,12 +4,5 @@ Created on Fri May 15 07:28:51 2020
 
 @author: salman
 """
-word = 'banana'
 
-count = 0
-
-for lame in word:
-    if lame=='a':
-        count = count + 1
-        
-print(count)        
+print('Dont play')     
