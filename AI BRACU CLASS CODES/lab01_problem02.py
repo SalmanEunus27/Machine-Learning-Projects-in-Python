382c3f705137620b20f7604f2fe8df3953f56e67
SalmanEunus27 <salmaneunus27@gmail.com>
2020-08-08 05:19:22 +0600

    done

---
+++ AI BRACU CLASS CODES/lab01_problem02.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  8 05:13:43 2020
+
+@author: salman
+"""
+c = float(input("Enter the temperature in Centigrade: "))
+f = (1.8*c) +32
+print("The Temperature in Fareinheit: ",f) 
+
