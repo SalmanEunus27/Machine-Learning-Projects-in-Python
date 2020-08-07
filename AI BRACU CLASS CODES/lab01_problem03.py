31c8cba133a0bc5a05d857c7204c083406c0a8e3
SalmanEunus27 <salmaneunus27@gmail.com>
2020-08-08 05:30:48 +0600

    done

---
+++ AI BRACU CLASS CODES/lab01_problem03.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  8 05:20:16 2020
+
+@author: salman
+"""
+l = int(input("Enter a whole number of length in inches: "))
+f = int(l/12)
+i = int(l%12)
+print(f,"feet and",i," inches")
