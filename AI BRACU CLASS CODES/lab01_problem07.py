1261a7dacfc724293a697df034f94b08bef87d5a
SalmanEunus27 <salmaneunus27@gmail.com>
2020-08-08 21:25:42 +0600

    done

---
+++ AI BRACU CLASS CODES/lab01_problem07.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  8 10:12:27 2020
+
+@author: salman
+"""
+
---
+++ AI BRACU CLASS CODES/lab01_problem08.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug  8 19:59:44 2020
+
+@author: salman
+"""
+list = [2,7,1,19,15,12]
+a = max(list)
+b = min(list)
+sum = a+b
+print("The sum of the smallest and largest number is: ",sum)
